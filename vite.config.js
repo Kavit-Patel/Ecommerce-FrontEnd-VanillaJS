9db50a14e2242d5556a983a7bd92e5724525a7b2
Kavit Patel <kvpatel.er@gmail.com>
2024-04-01 11:49:39 +0530

    vite.config.js edited

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3030,
+  },
   build: {
     rollupOptions: {
       input: {
